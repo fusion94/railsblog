930b3169b59750e55d6fe46d6a06a7728ded35a8
Tony Guntharp <fusion94@damagestudios.net>
2013-01-23 21:09:29 -0800

    first commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,3 @@
+To contribute, just issue a pull request. Include tests, please.
+
+If you add any new files please make sure you add the source header to the top of that file.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsblog::Application.config.secret_token = 'c6c6a689fad286ee9427b5915c49eaa963e540f2a0efcc7be87bd1379af124e5b1601907928d23edd6b69ecdf17bef9b7bcf30bad18c1cd98d4f916945d54e33'
